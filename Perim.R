e42fd3732387c868a4a581851e591c503be66814
Nerea Galera <negana@alumni.uv.es>
2026-02-02 20:18:29 +0100

    Correcion

--- Perim.R
+++ Perim.R
@@ -3,21 +3,7 @@ PerimetroCuadrado <- function(L){
 }
 
 LongitudCircunferencia <- function(radio){
-<<<<<<< HEAD
-  return(2 * pi * radio)
-}
-
-area_trapecio <- function(b, B, H) {
-  return((B + b) * H) / 2
-}
-
-
-PerimetroRectangulo <- function(base, altura) {
-  return((L+l)*2)
-}
-
-=======
-  return(2 * 3.14159 * radio)  # aquí sí va el 3.14159
+  return(2 * 3.14159 * radio)
 }
 
 area_trapecio <- function(b, B, H) {
@@ -25,8 +11,6 @@ area_trapecio <- function(b, B, H) {
 }
 
 PerimetroRectangulo <- function(L, l) {
-  return(2 * (L+l))
+  return(2 * (L + l))
 }
 
-
->>>>>>> Rama3
