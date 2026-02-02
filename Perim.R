012423becfc4b8d41c07343471ca5a4de326975e
Nerea Galera <negana@alumni.uv.es>
2026-02-02 20:11:08 +0100

    Sustituyo 2*(L+l)

--- Perim.R
+++ Perim.R
@@ -1,20 +1,17 @@
 PerimetroCuadrado <- function(L){
-  return(4*L)
+  return(4 * L)
 }
 
 LongitudCircunferencia <- function(radio){
-<<<<<<< HEAD
-  return(2 * pi * radio)
+  return(2 * 3.14159 * radio)  # aquí sí va el 3.14159
 }
 
 area_trapecio <- function(b, B, H) {
-  ((B + b) * H) / 2
-}
-=======
-  return(2 * 3.14159 * radio)
+  return((B + b) * H / 2)
 }
 
-PerimetroRectangulo <- function(base, altura) {
-  2 * (base + altura)
+PerimetroRectangulo <- function(L, l) {
+  return(2 * (L+l))
 }
->>>>>>> Rama2
+
+
